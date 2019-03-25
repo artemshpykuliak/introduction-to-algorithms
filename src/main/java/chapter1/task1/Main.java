caeae02cf43ef91f88504db5e02e0cce7d3287da
Artem Shpykuliak <shpykuliak@gmail.com>
2019-03-25 10:14:47 +0200

    First commit

---
+++ src/main/java/chapter1/task1/Main.java
@@ -0,0 +1,10 @@
+package chapter1.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        var array = new int[]{31, 41, 59, 26, 41, 58};
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
